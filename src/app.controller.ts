ebe8e26b06c9237c5dfc9c2a8d436c6b0cdbab24
welcomemonth <123719821+welcomemonth@users.noreply.github.com>
2023-02-12 10:27:25 +0800

    完善静态资源的提交

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render("default/index.ejs")
+  getHello() {
+    return {'name':"张三","age":"20"};
   }
 }
